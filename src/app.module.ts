c018bea3860949889cdcfebe1d8a7822e3f4f9cc
yejiayu <gailjy@126.com>
2024-10-17 09:14:04 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: yejiayu gailjy@126.com
+ * @Date: 2024-10-17 09:07:46
+ * @LastEditors: yejiayu gailjy@126.com
+ * @LastEditTime: 2024-10-17 09:12:07
+ * @FilePath: \todolist-backend\src\app.module.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { Module } from '@nestjs/common';
+
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
